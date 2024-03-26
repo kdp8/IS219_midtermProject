0652eddec1e4ee765122440ca40e3f0d1861c7d6
krishang <patel.krishang02@gmail.com>
2024-03-26 00:18:15 -0400

    Working menu feature

---
+++ app/plugins/menu/__init__.py
@@ -0,0 +1,13 @@
+import logging
+import os
+from app.commands import Command
+
+
+class MultiplyCommand(Command):
+    def execute(self):
+        try:
+            plugins_dir = os.path.join(os.getcwd(), "app", "plugins")
+            directories = [name for name in os.listdir(plugins_dir) if os.path.isdir(os.path.join(plugins_dir, name))]
+            print("List of available commands:", directories)
+        except FileNotFoundError:
+            logging.error("An unexpected error occured.")
