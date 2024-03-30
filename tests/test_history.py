f516298b0ba6fa7fe7d2c195fb1e620053b6a52b
krishang <patel.krishang02@gmail.com>
2024-03-29 22:03:47 -0400

    Refractor history test

--- tests/test_history.py
+++ tests/test_history.py
@@ -5,6 +5,7 @@ import re
 import unittest
 from unittest.mock import patch, mock_open
 import pandas as pd
+from app.env_vars import env
 from app.history import History
 from app.plugins.history import HistoryCommand
 
@@ -14,30 +15,33 @@ class TestHistory(unittest.TestCase):
     @patch('pandas.read_csv')
     def test_load_history(self, mock_read_csv):
         """Test loading history from a CSV file."""
+        expected_path = env.get_history_file_path('HISTORY_BASE_DIR', 'history.csv')
         mock_df = pd.DataFrame([['add', 1, 2]], columns=['command', 'param1', 'param2'])
         mock_read_csv.return_value = mock_df
 
         History.load_history()
 
-        mock_read_csv.assert_called_with('history.csv')
+        mock_read_csv.assert_called_with(expected_path)
 
     @patch('pandas.read_csv')
     @patch('pandas.DataFrame.to_csv')
     def test_delete_history(self, mock_to_csv, mock_read_csv):
         """Test deleting an entry from the history."""
+        expected_path = env.get_history_file_path('HISTORY_BASE_DIR', 'history.csv')
         mock_df = pd.DataFrame([['add', 1, 2], ['subtract', 3, 2]], columns=['command', 'param1', 'param2'])
         mock_read_csv.return_value = mock_df
 
         History.delete_history(0)
 
-        mock_to_csv.assert_called_with('history.csv', index=False)
+        mock_to_csv.assert_called_with(expected_path, index=False)
 
     @patch('builtins.open', new_callable=mock_open)
     def test_add_history(self, mock_file_open):
         """Test adding an entry to the history."""
+        expected_path = env.get_history_file_path('HISTORY_BASE_DIR', 'history.csv')
         History.add_history('multiply', 4, 5)
 
-        mock_file_open.assert_called_with('history.csv', 'a', newline='')
+        mock_file_open.assert_called_with(expected_path, 'a', newline='')
 
         written_content = mock_file_open().write.call_args[0][0]
         self.assertTrue(re.search('multiply,4,5', written_content))
@@ -45,9 +49,10 @@ class TestHistory(unittest.TestCase):
     @patch('builtins.open', new_callable=mock_open)
     def test_clear_history(self, mock_file_open):
         """Test clearing the history."""
+        expected_path = env.get_history_file_path('HISTORY_BASE_DIR', 'history.csv')
         History.clear_history()
 
-        mock_file_open.assert_called_with('history.csv', 'w', newline='')
+        mock_file_open.assert_called_with(expected_path, 'w', newline='')
 
         written_content = mock_file_open().write.call_args[0][0]
         self.assertTrue(re.search('command,param1,param2', written_content))
